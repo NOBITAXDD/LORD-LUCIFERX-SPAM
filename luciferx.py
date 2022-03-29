74085a6593a62c9ac0a0bd45b899a0d3d1ab7134
Ashmit rai <101203389+ashmitisop@users.noreply.github.com>
2022-03-29 14:46:04 +0530

    Create luciferx.py

---
+++ luciferx.py
@@ -0,0 +1,219 @@
+import os
+import sys
+import random
+from datetime import datetime
+from telethon import TelegramClient, events
+from telethon.sessions import StringSession
+from Config import STRING, SUDO, BIO_MESSAGE, API_ID, API_HASH, STRING2, STRING3, STRING4 ,STRING5, STRING6, STRING7, STRING8 ,STRING9, STRING10, STRING11, STRING12, STRING13, STRING14, STRING15, STRING16, STRING17, STRING18, STRING19, STRING20, STRING21, STRING22, STRING23, STRING24, STRING25, HEROKU_API_KEY, HEROKU_APP_NAME
+import asyncio
+import telethon.utils
+from telethon.tl import functions
+from telethon.tl.functions.channels import LeaveChannelRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest
+from Utils import RAID, RRAID
+import git
+import heroku3
+
+
+a = API_ID
+b = API_HASH
+smex = STRING
+smexx = STRING2
+smexxx = STRING3
+smexxxx = STRING4
+smexxxxx = STRING5
+sixth = STRING6
+seven = STRING7
+eight = STRING8
+ninth = STRING9
+tenth = STRING10
+eleve = STRING11
+twelv = STRING12
+thirt = STRING13
+forte = STRING14
+fifth = STRING15
+sieee = STRING16
+seeee = STRING17
+eieee = STRING18
+nieee = STRING19
+gandu = STRING20
+ekish = STRING21
+baish = STRING22
+teish = STRING23
+tfour = STRING24
+tfive = STRING25
+
+
+idk = ""
+ydk = ""
+wdk = ""
+sdk = ""
+hdk = ""
+adk = ""
+bdk = ""
+cdk = ""
+edk = ""
+ddk = ""
+vkk = ""
+kkk = ""
+lkk = ""
+mkk = ""
+sid = ""
+shy = ""
+aan = ""
+ake = ""
+eel = ""
+khu = ""
+shi = ""
+yaa = ""
+dav = ""
+raj = ""
+put = ""
+
+
+que = {}
+
+SMEX_USERS = []
+for x in SUDO: 
+    SMEX_USERS.append(x)
+    
+async def start_yukki():
+    global idk
+    global ydk
+    global wdk
+    global sdk
+    global hdk
+    global adk
+    global bdk
+    global cdk
+    global ddk
+    global edk
+    global vkk
+    global kkk
+    global lkk
+    global mkk
+    global sid
+    global shy
+    global aan
+    global ake
+    global eel
+    global khu
+    global shi
+    global yaa
+    global dav
+    global raj
+    global put
+    
+    if smex:
+        session_name = str(smex)
+        print("String 1 Found")
+        idk = TelegramClient(StringSession(session_name), a, b)
+        try:
+            print("Booting Up The Client 1")
+            await idk.start()
+            botme = await idk.get_me()
+            botid = telethon.utils.get_peer_id(botme)
+            SMEX_USERS.append(botid)
+        except Exception as e:
+            idk = "smex"
+            print(e)
+            pass
+    else:
+        print("Session 1 not Found")
+        session_name = "startup"
+        idk = TelegramClient(session_name, a, b)
+        try:
+            await idk.start()
+        except Exception as e:
+            pass
+   
+    if smexx:
+        session_name = str(smexx)
+        print("String 2 Found")
+        ydk = TelegramClient(StringSession(session_name), a, b)
+        try:
+            print("Booting Up The Client 2")
+            await ydk.start()
+            botme = await ydk.get_me()
+            botid = telethon.utils.get_peer_id(botme)
+            SMEX_USERS.append(botid)
+        except Exception as e:
+            print(e)
+            pass
+    else:
+        print("Session 2 not Found")
+        pass
+        session_name = "startup"
+        ydk = TelegramClient(session_name, a, b)
+        try:
+            await ydk.start()
+        except Exception as e:
+            pass
+
+    if smexxx:
+        session_name = str(smexxx)
+        print("String 3 Found")
+        wdk = TelegramClient(StringSession(session_name), a, b)
+        try:
+            print("Booting Up The Client 3")
+            await  wdk.start()
+            botme = await wdk.get_me()
+            botid = telethon.utils.get_peer_id(botme)
+            SMEX_USERS.append(botid)
+        except Exception as e:
+            print(e)
+            pass
+    else:
+        print("Session 3 not Found")
+        pass
+        session_name = "startup"
+        wdk = TelegramClient(session_name, a, b)
+        try:
+            await wdk.start()
+        except Exception as e:
+            pass
+if smexxxx:
+        session_name = str(smexxxx)
+        print("String 4 Found")
+        hdk = TelegramClient(StringSession(session_name), a, b)
+        try:
+            print("Booting Up The Client 4")
+            await hdk.start()
+            botme = await hdk.get_me()
+            botid = telethon.utils.get_peer_id(botme)
+            SMEX_USERS.append(botid)
+        except Exception as e:
+            print(e)
+            pass
+    else:
+        print("Session 4 not Found")
+        pass
+        session_name = "startup"
+        hdk = TelegramClient(session_name, a, b)
+        try:
+            await hdk.start()
+        except Exception as e:
+            pass
+
+    if smexxxxx:
+        session_name = str(smexxxxx)
+        print("String 5 Found")
+        sdk = TelegramClient(StringSession(session_name), a, b)
+        try:
+            print("Booting Up The Client 5")
+            await sdk.start()
+            botme = await sdk.get_me()
+            botid = telethon.utils.get_peer_id(botme)
+            SMEX_USERS.append(botid)
+        except Exception as e:
+            print(e)
+            pass
+    else:
+        print("Session 5 not Found")
+        pass
+        session_name = "startup"
+        sdk = TelegramClient(session_name, a, b)
+        try:
+            await sdk.start()
+        except Exception as e:
+            pass
